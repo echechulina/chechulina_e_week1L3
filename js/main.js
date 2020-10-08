d54d841d3d5f74344301e22764edfb1238789727
Elena Chechulina <elenachechulina@Elenas-MacBook-Pro.local>
2020-10-08 14:19:09 -0400

    adding dynamic data to our view

--- js/main.js
+++ js/main.js
@@ -1,10 +1,29 @@
 // import your packages here
-import Person from "./modules/DataModule.js";
-import NavSystem from "./modules/TheNavSystem.js";
+import Team from "./modules/DataModule.js"
 
 (() => {
     // stub * just a place for non-component-specific stuff
-    console.log('loaded');
+    let userSection = document.querySelector(".user-section"),
+        userTemplate = document.querySelector("#profs-template").content;
 
-    console.log(Person);
+    debugger;
+
+    function handleDataSet(data) {
+
+        for (let user in data) {
+            let currentUser = userTemplate.cloneNode(true),
+                currentUserText = currentUser.querySelector('.user').children;
+
+        currentUserText[1].textContent = data[user].name;
+        currentUserText[2].textContent = data[user].role;
+        currentUserText[3].textContent = data[user].nickname;
+
+        userSection.appendChild(currentUser);
+
+        }
+       
+    }
+
+    handleDataSet();
+
 })();
--- js/modules/DataModule.js
+++ js/modules/DataModule.js
@@ -1,5 +1,32 @@
-module.exports = {
+export default {
+    Trevor:{
     name: "Trevor",
-    role: "Prof",
+    role: "Killer",
     nickname: "Tervor"
+    }, 
+
+    John:{
+        name: "John",
+        role: "super chill prof",
+        nickname: "Johnhnhr"
+        }, 
+
+    Joe:{
+    name: "Joe",
+    role: "Prof",
+    nickname: "Teddy Bear"
+    }, 
+
+    Pan:{
+        name: "Pan",
+        role: "Code ninja",
+        nickname: "Spider Pan"
+        }, 
+
+        Jarrod:{
+            name: "Jarrod",
+            role: "Male Model Prof",
+            nickname: "Zoolander"
+            }
+
 }
