506b98d10dfd515f056c4d6b477ccaae7bb36736
Elena Chechulina <elenachechulina@Elenas-MacBook-Pro.local>
2020-10-29 12:06:29 -0400

    adding datat miner

--- js/main.js
+++ js/main.js
@@ -1,34 +1,40 @@
+// import your packages here
+import { fetchData, postData } from "./modules/TheDataMiner.js";
+
 (() => {
-    fetch('./DataSet.json')
-        .then (res => res.json())
-        .then (data => {
-            handleDataSet(data);
-        })
+    // stub * just a place for non-component-specific stuff
+    console.log('loaded');
+    
+    function popErrorBox(message) {
+        alert("Something has gone horribly, horribly wrong");
+    }
 
-    .catch((error) => console.log(error));
-
-
-    // this receives the data payload from our AJAX request, parses it (turns the returned JSON object back into a plain JavaScript object) and renders the data to our view (the markup in index.html)
     function handleDataSet(data) {
         let userSection = document.querySelector('.user-section'),
             userTemplate = document.querySelector('#user-template').content;
-
-        debugger;
-
-        // loop through the JavaScript object and for each user, make a copy of the user template we find at the bottom of index.html, populate it with the user's data, and put that fresh copy in the users section in index.html
 
         for (let user in data) {
             let currentUser = userTemplate.cloneNode(true),
                 currentUserText = currentUser.querySelector('.user').children;
 
-            currentUserText[1].textContent = data[user].name;
-            currentUserText[2].textContent = data[user].role;
-            currentUserText[3].textContent = data[user].nickname;
+            currentUserText[1].src = `images/${data[user].biopic}`;
+            currentUserText[2].textContent = data[user].name;
+            currentUserText[3].textContent = data[user].role;
+            currentUserText[4].textContent = data[user].nickname;
 
             // add this new user to the view
             userSection.appendChild(currentUser);
         }
-
-        console.log(data);
     }
+
+    
+    // we can add a catch handler to a thenable if things go wrong during our data retrieval attempt
+    // really, we should move all of this to an external class or function and pass arguments into it.
+
+    // that would make it really flexible and able to handle all kinds of requests and we could pass in a callback depending on what we want to do with our data
+
+    // but then we'd be on our way to rewriting the Axios API (you should research it)
+    fetchData("../DataSet.json").then(data => handleDataSet(data)).catch(err => { console.log(err); popErrorBox(err); });
+    
+    postData("test").then(data => console.log(data)).catch(err => console.log(err));
 })();
